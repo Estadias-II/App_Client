4c94a1c48ae35b3c3f1e637a7a015431ef62f335
BlueVelociraptor <bluevelociraptor1555@gmail.com>
2025-12-01 12:53:47 -0500

    Actualizacion de servicios de deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Importante para Netlify
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          ui: ['axios', 'react-toastify']
+        }
+      }
+    }
+  }
 })
